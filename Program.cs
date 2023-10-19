1b3c8fcd3da8081a6fb65ed1cd57317332982b0d
Tamer <tamer.masud@mail.ru>
2023-10-19 20:52:36 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BooleanVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
