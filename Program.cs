ceffb5f2eba1119eef1d3af421ebd776cde7d71e
Tamer <tamer.masud@mail.ru>
2023-10-19 21:58:35 +0400

    Добавлен case для цвета Cyan, а так же изменен default.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,68 @@
-﻿namespace BooleanVariables
+﻿/*
+Задание 4.1.4
+string a, b;
+bool c = a != b;
+
+Задание 4.1.5
+int a, b;
+double x, y;
+bool c = (a < b) || (x > y);
+*/
+
+/*
+Задание 4.1.12
+var a = 6;
+var b = 7;
+
+if (a < b)
 {
-    internal class Program
+    Console.WriteLine("Условие истинно");
+}
+else
+{
+    Console.WriteLine("Условие ложно");
+}
+*/
+
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+        var color = Console.ReadLine();
+
+        switch (color)
         {
-            Console.WriteLine("Hello, World!");
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+                break;
+
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+                break;
+
+            case "cyan":
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+                break;
+
+            default:
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("Your color is yellow!");
+                break;
         }
     }
 }
