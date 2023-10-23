f9c266355b71b2711ee43a3776bd2bf471a178c9
Tamer <tamer.masud@mail.ru>
2023-10-23 14:38:08 +0400

    Добавлен  оператр if для остановки цикла и оператор continue в ператор swith.

--- Program.cs
+++ Program.cs
@@ -204,6 +204,7 @@ class MainClass
 }
 */
 
+/*
 using System;
 
 class MainClass
@@ -328,5 +329,60 @@ class MainClass
                     break;
             }
         } while (true);
+    }
+}
+*/
+
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int k = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            Console.WriteLine(k);
+
+            var text = Console.ReadLine();
+
+            if (text == "stop")
+            {
+                Console.WriteLine("Цикл остановлен");
+                break;
+            }
+
+            switch (text)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+
+                default:
+                    continue;
+            }
+
+            k++;
+
+        }
     }
 }
