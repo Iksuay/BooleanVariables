e305e944665e6732000a26e97ada9546cad471b6
Tamer <tamer.masud@mail.ru>
2023-10-23 11:45:51 +0400

    Добавлен цикл for

--- Program.cs
+++ Program.cs
@@ -32,37 +32,38 @@ class MainClass
     {
         Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-        var color = Console.ReadLine();
-
-        switch (color)
+        for (;;)
         {
-            case "red":
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
+            switch (Console.ReadLine())
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
 
-                Console.WriteLine("Your color is red!");
-                break;
+                    Console.WriteLine("Your color is red!");
+                    break;
 
-            case "green":
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
 
-                Console.WriteLine("Your color is green!");
-                break;
+                    Console.WriteLine("Your color is green!");
+                    break;
 
-            case "cyan":
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
 
-                Console.WriteLine("Your color is cyan!");
-                break;
+                    Console.WriteLine("Your color is cyan!");
+                    break;
 
-            default:
-                Console.BackgroundColor = ConsoleColor.Yellow;
-                Console.ForegroundColor = ConsoleColor.Red;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
 
-                Console.WriteLine("Your color is yellow!");
-                break;
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
         }
     }
 }
