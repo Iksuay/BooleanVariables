957b8c547cb38e5a00a077b0fde1b3446826ecda
Tamer <tamer.masud@mail.ru>
2023-10-24 10:47:09 +0400

    Задание 4.3.11

--- Program.cs
+++ Program.cs
@@ -477,7 +477,7 @@ namespace FirstApp
 }
 */
 
-
+/*
 using System;
 
 namespace FirstApp
@@ -494,6 +494,43 @@ namespace FirstApp
             {
                 Console.Write(name[i] + " ");
             }
+        }
+    }
+}
+*/
+
+/*
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };
+        
+        foreach (int i in array)
+        {
+            Console.WriteLine(i + " ");
+        }
+    }
+}
+*/
+
+
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int[,] array = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+
+        for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+        {
+            for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+                Console.Write(array[k, i] + " ");
+
+            Console.WriteLine();
         }
     }
 }
