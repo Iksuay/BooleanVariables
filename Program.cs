39065b6aae957158ef7ee51f6be7d81b23b1c230
Tamer <tamer.masud@mail.ru>
2023-10-24 17:13:00 +0400

    Задание 4.4.3

--- Program.cs
+++ Program.cs
@@ -647,6 +647,7 @@ for (int i = 0; i <= arr.GetUpperBound(0); i++)
 }
 */
 
+/* Задание 4.4.2
 (string name, int age) anketa;
 
 Console.WriteLine("Введите свое имя: ");
@@ -656,3 +657,18 @@ anketa.Item2 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Ваше имя: {0}", anketa.Item1);
 Console.WriteLine("Ваш возраст: {0}", anketa.Item2);
+*/
+
+
+var (name, age) = ("Тамер", 25);
+Console.WriteLine("Моё имя: {0}", name);
+Console.WriteLine("Мой возраст: {0}", age);
+
+
+Console.Write("Введите имя: "); 
+name = Console.ReadLine();
+Console.Write("Введите возраст с цифрами: ");
+age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ваше имя: {0}", name);
+Console.WriteLine("Ваш возраст: {0}", age);
